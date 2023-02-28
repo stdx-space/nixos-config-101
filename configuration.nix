63fea860cd0fe0a08d4e21a63e0b9218db7d3e87
Tommy Li <dev@stdx.space>
2023-02-28 20:32:39 +0800

    enable flake

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,8 @@
     storageDriver = "btrfs";
   };
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
