e49ac7102dd97594ef0eb0aea2a204abd17a4497
Tommy Li <dev@stdx.space>
2023-03-06 01:32:11 +0800

    port assignment

--- modules/stdx-code-server.nix
+++ modules/stdx-code-server.nix
@@ -15,8 +15,16 @@ let
       };
     };
   };
-  host = "127.0.0.1";
-  port = 8081;
+
+  # assign port consecutively from cfg.port to cfg.port + no_of_instances - 1
+  instancesCfg = lib.listToAttrs
+    (lib.imap0
+      (i: v: lib.nameValuePair v.name (v.value // {
+        port = cfg.port + i;
+      }))
+      (lib.mapAttrsToList
+        (k: v: lib.nameValuePair k v)
+        cfg.instances));
 in
 {
   options.services.stdx-code-server = {
@@ -39,7 +47,7 @@ in
       default = 28000;
       description = lib.mdDoc ''
         The port where the first user's code-server runs.
-        The port range port..port+no_of_user-1 will be reserved.
+        The port range port..port+no_of_instances-1 will be reserved.
       '';
       type = lib.types.port;
     };
@@ -69,12 +77,12 @@ in
             wantedBy = [ "multi-user.target" ];
             after = [ "network-online.target" ];
             serviceConfig = {
-              ExecStart = "${cfg.package}/bin/code-server --bind-addr ${host}:${toString port} " + lib.escapeShellArgs cfg.extraArguments;
+              ExecStart = "${cfg.package}/bin/code-server --bind-addr ${cfg.host}:${toString instanceCfg.port} " + lib.escapeShellArgs cfg.extraArguments;
               Restart = "on-failure";
               User = instanceCfg.user;
             };
           })
-          cfg.instances;
+          instancesCfg;
       services.caddy.virtualHosts =
         lib.mapAttrs'
           (name: instanceCfg: lib.nameValuePair "code.stdx.space:80/${instanceCfg.user}" {
@@ -82,15 +90,15 @@ in
               redir http://code.stdx.space/${instanceCfg.user}/
             '';
           })
-          cfg.instances //
+          instancesCfg //
         lib.mapAttrs'
           (name: instanceCfg: lib.nameValuePair "code.stdx.space:80/${instanceCfg.user}/*" {
             extraConfig = ''
               uri strip_prefix /${instanceCfg.user}
-              reverse_proxy ${host}:${toString port}
+              reverse_proxy ${cfg.host}:${toString instanceCfg.port}
             '';
           })
-          cfg.instances
+          instancesCfg
       ;
     };
 }
